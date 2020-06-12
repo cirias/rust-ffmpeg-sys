ff38ae1b51649ea6abfa1a7ce2e6c68b59607393
Sirius <sirius@x250>
2020-06-12 13:05:59 +0800

    Allow improper_ctypes

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+// Due to https://github.com/rust-lang/rust-bindgen/issues/1549
+#![allow(improper_ctypes)]
+
 extern crate libc;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
